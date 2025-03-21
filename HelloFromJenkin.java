2a2aead2333a7b02403f1b1acf0fa7350e0977ef
Kamlesh Pant <kkpant75@yahoo.com>
2025-03-21 16:41:31 +0530

    test jenkin auto

--- HelloFromJenkin.java
+++ HelloFromJenkin.java
@@ -1,5 +1,5 @@
 public class HelloFromJenkin {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins! Rub This Change Automatic test Trigger");
+        System.out.println("Hello from Jenkins! Rub This Change Automatic test");
     }
 }
